f32b25fe6fed2f9dcd8cd68a68eb5421d09409c6
mwood77 <43637076+mwood77@users.noreply.github.com>
2024-02-11 17:24:38 +0100

    add working doc checker jobs

--- .github/workflows/test-ali-links.mjs
+++ .github/workflows/test-ali-links.mjs
@@ -1,9 +1,12 @@
 import { readFile } from 'fs/promises';
 import { By, Builder } from 'selenium-webdriver';
-import firefox from 'selenium-webdriver/firefox.js'
+import Firefox from 'selenium-webdriver/firefox.js'
 
 const links = [];
 let driver;
+
+const bomDocument = './docs/bom-requirements.md';
+const bomDocumentPathForLocalTesting = '../../docs/bom-requirements.md';
 
 const content = async(file) => {
     return await readFile(file, 'utf8')
@@ -11,14 +14,19 @@ const content = async(file) => {
 
 const search = async (item) => {
     await driver.get('https://s.click.aliexpress.com/e/_' + item);
-    return await driver.findElements(By.className('not-found-page'))
+    let notFound = await driver.findElements(By.className('not-found-page'))    // Product not found
+    let homepage = await driver.findElements(By.className('new-affiliate'))     // Link points to homepage
+    const results = await Promise.all([notFound, homepage])
+
+    return results.flat(Infinity);
 }
 
-content('./instructions/bom-requirements.md')
+content(bomDocument)
     .then(
         result => {
             const sanitizedMD = result.split('/_')
-            sanitizedMD.shift(); // remove stuff that doesn't include links
+            // remove stuff that doesn't include links
+            sanitizedMD.shift();
             sanitizedMD.forEach((el) => {
                 links.push(el.split(')')[0])
             })
@@ -26,14 +34,20 @@ content('./instructions/bom-requirements.md')
     )
     .finally(
         async () => {
+            
+            const options = new Firefox.Options();
+            options.addArguments('--headless');
+
             driver = new Builder()
                 .forBrowser('firefox')
-                .setFirefoxOptions(new firefox.Options().headless())
+                .setFirefoxOptions(options)
                 .build();
 
             // Test scraped aliexpress links from bom-doc
             links.every(async el => {
                 const result = await search(el);
+
+                console.log(result)
 
                 if (result.length > 0) {
                     throw new Error(`product ${el} is a broken link`, content)
